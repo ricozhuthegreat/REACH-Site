9a86bed1562024c79b91ce678d59067027c7e417
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-06-17 11:01:17 -0400

    Quiz Function Update
     - It now works witha  fixed user input

--- Quiz.js
+++ Quiz.js
@@ -1,3 +1,9 @@
-<Script>
-
-</Script>
+function check() {
+  var ans = document.quiz.user.value;
+  document.getElementById("answer").style.visibility = "visible";
+  if (ans == "Ottawa") {
+    document.getElementById("message").innerHTML = "Correct!";
+  } else {
+    document.getElementById("message").innerHTML = "Incorrect!";
+  }
+}
