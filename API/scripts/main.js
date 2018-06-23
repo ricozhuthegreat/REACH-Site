820aab9a32a86d8a2585e63f6da099855f5587b6
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-06-23 17:20:09 -0400

    Reorganization and Added Image
     - New JPEG Image in images folder named schoolOTHS.jpg
     - Reorganized js

--- API/scripts/main.js
+++ API/scripts/main.js
@@ -1,10 +1,21 @@
 
+var mainImg = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/schoolOTHS');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
 function getQFromFile(fileName) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", fileName, false);
     rawFile.onreadystatechange = function () {
-        if(rawFile.readyState == 4) {
-            if(rawFile.status == 200 || rawFile.status == 0) {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
                 console.log(allText);
             } else {
@@ -18,7 +29,7 @@ function getQFromFile(fileName) {
 function check () {
   var ans = document.quiz.user.value;
   document.getElementById("answer").style.visibility = "visible";
-  if (ans == "answer") {
+  if (ans === "answer") {
     document.getElementById("message").innerHTML = "Correct!";
   } else {
     document.getElementById("message").innerHTML = "Incorrect!";
