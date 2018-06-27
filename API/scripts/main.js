783adfc618e7e190f5149ff920fb8c7f37310545
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2018-06-27 14:52:17 -0400

    Improvements of the question reader
     - Finished implementing the basic js framework for the questions reader

--- API/scripts/main.js
+++ API/scripts/main.js
@@ -14,40 +14,6 @@ window.onload = function() {
 
 }
 
-function readFile () {
-  // var xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     document.getElementById("testF").innerHTML =
-  //     this.responseText;
-  //   }
-  // };
-  // xhttp.open("GET", "test1.txt", true);
-  // xhttp.send();
-    var client = new XMLHttpRequest();
-    client.open('GET', 'test1.txt');
-    client.onreadystatechange = function() {
-      document.getElementById("testF").innerHTML = client.responseText;
-    }
-    client.send();
-}
-
-function getQFromFile (fileName) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", fileName, false);
-    rawFile.onreadystatechange = function () {
-        if(rawFile.readyState === 4) {
-            if(rawFile.status === 200 || rawFile.status == 0) {
-                var allText = rawFile.responseText;
-                console.log(allText);
-            } else {
-                console.log(rawFile.status);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
 function check () {
   var ans = document.quiz.user.value;
   document.getElementById("answer").style.visibility = "visible";
---
+++ API/scripts/questions.js
@@ -0,0 +1,26 @@
+
+function renderQuestions (questions) {
+      window.alert("There");
+    var mainQDivRef = document.getElementById("mainQ");
+    var mainQDiv = document.createElement("div");
+
+    mainQDiv.innerHTML = questions.question;
+    mainQDivRef.appendChild(mainQDiv);
+}
+
+function getQuestions () {
+    var req = new XMLHttpRequest();
+  //  req.onload() = function () {
+
+  //  };
+    req.open("GET", "questions.json", true);
+    window.alert(req.responseText);
+    var questions = JSON.parse(req.responseText);
+    getQ(questions);
+    req.send(null);
+}
+
+function getQ (questions) {
+      window.alert("Hello");
+    renderQuestions(questions);
+}
